0249020e3c0166a6dc5da3ae80611253632f9ed7
Ashley Valentine <avalentinexo@gmail.com>
2023-01-10 15:06:52 -0500

    moved DB connection for serverless hosting

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const PORT = 1337
-const mongoose = require("mongoose")
-const passport = require("passport")
-const session = require("express-session")
-const MongoStore = require("connect-mongo")(session)
-const flash = require("express-flash")
-const logger = require("morgan")
-const moment = require("moment")
-const connectDB = require("./config/database")
-const mainRoutes = require("./routes/main")
-const dashboardRoutes = require("./routes/dashboard")
-const checkinRoutes = require("./routes/checkin")
-require("dotenv").config({ path: "./config/.env"})
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const moment = require('moment')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const dashboardRoutes = require('./routes/dashboard')
+const checkinRoutes = require('./routes/checkin')
+require('dotenv').config({ path: './config/.env' })
 
 // Passport
-require("./config/passport")(passport)
-
-connectDB()
+require('./config/passport')(passport)
 
 // Set Middleware
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(logger("dev"))
+app.use(logger('dev'))
 
 // Sessions
 app.use(
-    session({
-        secret: "keyboard cat",
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
 )
 
 // Passport Middleware
@@ -43,8 +41,12 @@ app.use(passport.session())
 app.use(flash())
 
 // Set Routes
-app.use("/", mainRoutes)
-app.use("/dashboard", dashboardRoutes)
-app.use("/checkin", checkinRoutes)
+app.use('/', mainRoutes)
+app.use('/dashboard', dashboardRoutes)
+app.use('/checkin', checkinRoutes)
 
-app.listen(process.env.PORT || PORT, ()=> console.log(`Server is running on port ${PORT}`))
+connectDB().then(() => {
+  app.listen(process.env.PORT || PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+  )
+})
